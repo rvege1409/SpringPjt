71281faeb126b4e8b22fca39ca8aa6960a253335
ChangWoo Choi <rvege1409@gmail.com>
2018-12-20 17:10:41 +0900

    complete business login

---
+++ src/main/java/org/zerock/service/BoardServiceImpl.java
@@ -0,0 +1,53 @@
+package org.zerock.service;
+
+import java.util.List;
+
+import org.springframework.stereotype.Service;
+import org.zerock.domain.BoardVO;
+import org.zerock.mapper.BoardMapper;
+
+import lombok.AllArgsConstructor;
+import lombok.Setter;
+import lombok.extern.log4j.Log4j;
+
+
+@Log4j
+@Service
+@AllArgsConstructor
+public class BoardServiceImpl implements BoardService {
+
+	private BoardMapper mapper;
+	
+	@Override
+	public void register(BoardVO board) {
+		log.info("register...." +board);
+		mapper.insertSelectKey(board);
+	}
+
+	@Override
+	public BoardVO get(Long bno) {
+		log.info("get...." );
+		mapper.read(bno);
+		return null;
+	}
+
+	@Override
+	public boolean modify(BoardVO board) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public boolean remove(Long bno) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public List<BoardVO> getList() {
+		log.info("getList.......");
+		mapper.getList();
+		return null;
+	}
+
+}
